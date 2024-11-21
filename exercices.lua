160738a2e219465a4ca769e46f65126d5eea7d55
RetrozDev <ryanbarbozapro@gmail.com>
2024-11-21 15:35:04 +0100

    Exercice 1 terminé

--- exercices.lua
+++ exercices.lua
@@ -1,5 +1,6 @@
 -- Afficher "Bonjour, Lua!" dans la console.
 -- Code en dessous:
+print("Exo 1: \nBonjour, Lua!")
 
 -- Addition simple
 -- Code en dessous:
@@ -8,14 +9,11 @@
 -- Demander un nombre à l'utilisateur et indiquer s'il est pair ou impair.
 -- Code en dessous:
 
-
 -- Afficher les nombres de 1 à 10 en utilisant une boucle.
 -- Code en dessous:
 
-
 -- Créer un tableau de nombres, calculer et afficher leur somme.
 -- Code en dessous:
-
 
 -- Écrire une fonction qui prend un tableau de nombres et retourne le plus grand.
 -- Code en dessous:
@@ -23,14 +21,11 @@
 -- Demander un nombre à l'utilisateur et calculer sa factorielle.
 -- Code en dessous:
 
-
 -- Créer un mini-jeu où l'utilisateur doit deviner un nombre choisi aléatoirement par le programme.
 -- Code en dessous:
 
-
 -- Implémenter un algorithme de tri (par exemple, tri à bulles) pour un tableau donné.
 -- Code en dessous:
-
 
 -- Créer un petit système d'inventaire où l'utilisateur peut ajouter, supprimer ou afficher des objets via des commandes.
 -- Code en dessous:
